72652c09bef15185223545970d6e9066a68aee7c
primehubpk <primehubpk1@gmail.com>
2026-08-15 12:05:26 +0500

    Place weekly deal status and More Weekly Deals below product details

--- app/product/[id]/layout.tsx
+++ app/product/[id]/layout.tsx
@@ -2,5 +2,5 @@ import ProductRewardInfo from '@/components/ProductRewardInfo';
 import WeeklyDealProductExtras from '@/components/WeeklyDealProductExtras';
 
 export default function ProductLayout({children,params}:{children:React.ReactNode;params:{id:string}}){
- return <><ProductRewardInfo productId={params.id}/><WeeklyDealProductExtras productId={params.id}/>{children}</>;
+ return <><ProductRewardInfo productId={params.id}/>{children}<WeeklyDealProductExtras productId={params.id}/></>;
 }
