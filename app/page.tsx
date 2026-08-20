f0e8629667f838c52c1b01df511cd026ef68a54f
primehubpk <primehubpk1@gmail.com>
2026-08-20 07:05:23 +0500

    Revert duplicate WeeklyDealStrip import from Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import Header from '@/components/Header';
 import HeroFlashBanner from '@/components/HeroFlashBanner';
-import WeeklyDealStrip from '@/components/WeeklyDealStrip';
 import PriceBuckets from '@/components/PriceBuckets';
 import ProductGridRewards from '@/components/ProductGridRewards';
 import YouTubeGuide from '@/components/YouTubeGuide';
@@ -26,7 +25,6 @@ export default function HomePage() {
       <div className="min-h-screen bg-[#F4F4F1] text-[#14140F]">
         <Header />
         <HeroFlashBanner />
-        <WeeklyDealStrip />
         <PriceBuckets selectedMaxPrice={selectedMaxPrice} wholesaleSelected={wholesaleSelected} onSelect={selectPrice} onWholesaleSelect={selectWholesale} />
         <ProductGridRewards selectedMaxPrice={selectedMaxPrice} wholesaleSelected={wholesaleSelected} />
         <YouTubeGuide />
