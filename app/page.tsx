986b36cd3b11973e2ca75e1449ed2b4ca3b65ee8
primehubpk <primehubpk1@gmail.com>
2026-09-03 11:41:15 +0500

    Phase 5: show Prime Skills rail on home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import CategorySwiper from '@/components/CategorySwiper';
 import PriceBuckets from '@/components/PriceBuckets';
 import NewArrivalsRail from '@/components/NewArrivalsRail';
 import ProductGridRewards from '@/components/ProductGridRewards';
+import PrimeSkillsHomeRail from '@/components/home/PrimeSkillsHomeRail';
 import YouTubeGuide from '@/components/YouTubeGuide';
 import Footer from '@/components/Footer';
 
@@ -33,6 +34,7 @@ export default function HomePage() {
         <div id="discover-deals-section">
           <ProductGridRewards selectedMaxPrice={selectedMaxPrice} wholesaleSelected={wholesaleSelected} />
         </div>
+        <PrimeSkillsHomeRail />
         <YouTubeGuide />
         <Footer onWholesaleSelect={selectWholesale} />
       </div>
